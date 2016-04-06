805e481957202b85dfcecb332f8817b519168c01
Cedric Stoquer <cstoquer@wizcorp.jp>
2016-04-06 22:47:50 +0900

    dialogs

--- build/index.js
+++ build/index.js
@@ -6777,12 +6777,19 @@ Bob.prototype.draw = function () {
 },{"./Level.js":39}],38:[function(require,module,exports){
 var level = require('./Level.js');
 var bob   = require('./Bob.js');
+
 var background = new Map();
+var textWindow = new Texture(64, 19).pen(10);
 
 var TILE_WIDTH  = settings.spriteSize[0];
 var TILE_HEIGHT = settings.spriteSize[1];
 var GRAVITY     = 0.5;
 var MAX_GRAVITY = 2;
+
+
+var nextLevel, nextDoor, inTransition, transitionCount, nextSide;
+var displayedText = false;
+var textParts = [];
 
 //▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
 function GameController() {
@@ -6807,7 +6814,6 @@ GameController.prototype.loadLevel = function (id, doorId, side) {
 };
 
 //▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-var nextLevel, nextDoor, inTransition, transitionCount, nextSide;
 GameController.prototype.changeLevel = function (id, doorId) {
 	inTransition = true;
 	transitionCount = -30;
@@ -6828,6 +6834,33 @@ GameController.prototype.goToSideLevel = function (direction) {
 };
 
 //▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+GameController.prototype._displayText = function () {
+	camera(0, 0);
+	draw(textWindow, 0, 0); // TODO character by character animation
+	if (btnp.A) {
+		if (textParts.length === 0) {
+			displayedText = false;
+			return;
+		}
+		textWindow.println(textParts.shift());
+	}
+};
+
+GameController.prototype.displayText = function (text) {
+	textWindow.cls();
+	displayedText = true;
+	// split text
+	textParts = text.split('\n');
+	// TODO check each line length and further split if too long
+	// print the first 3 lines
+	for (var i = 0; i < 3; i++) {
+		if (textParts.length === 0) break;
+		textWindow.println(textParts.shift());
+	}
+};
+
+
+//▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
 GameController.prototype.ditherTransition = function () {
 	camera(0, 0);
 	draw(assets.ditherFondu, 0, transitionCount * TILE_HEIGHT);
@@ -6839,9 +6872,10 @@ GameController.prototype.ditherTransition = function () {
 
 //▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
 GameController.prototype.update = function () {
-	if (inTransition) return this.ditherTransition();
+	if (inTransition)  return this.ditherTransition();
+	if (displayedText) return this._displayText();
 
-	
+	if (btnp.B) return this.displayText(assets.dialogs.bobIntro);
 
 	bob.update();
 
